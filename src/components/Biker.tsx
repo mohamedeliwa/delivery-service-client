e06954903c1c3bc3c6a6e0eae085f32129994d36
mohamedeliwa <mohamed.av11@gmail.com>
2023-11-30 17:12:40 +0200

    fetching a list of all parcels for a biker

--- src/components/Biker.tsx
+++ src/components/Biker.tsx
@@ -1,47 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Biker.module.css";
 import { User } from "@/types/user.types";
+import { Parcel } from "@/types/parcel.types";
+import axios from "axios";
 
 const Biker: React.FC<{ token: string; user: User | null }> = ({
   token,
   user,
 }) => {
+  const [parcels, setParcels] = useState<Parcel[]>([]);
+
+  useEffect(() => {
+    if (parcels.length === 0) {
+      axios
+        .get(`http://localhost:8000/parcels`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          setParcels([...response.data]);
+        });
+    }
+  }, [parcels.length, token, user?.id]);
+  
+
   return (
     <div className={styles.container}>
       Biker
       <div>
         <h3>List of All Parcels</h3>
-        <div className={styles.parcel}>
-          <span>Name: Jhone Deo</span>
-          <span>Pick-up: London </span>
-          <span>Dropp-off: NY</span>
-          <span>Biker: 1</span>
-          <span>Status: picked up</span>
-          <span>
-            <button className={styles.button}>Take</button>
-          </span>
-        </div>
-
-        <div className={styles.parcel}>
-          <span>Name: Jhone Deo</span>
-          <span>Pick-up: London </span>
-          <span>Dropp-off: NY</span>
-          <span>Biker: 1</span>
-          <span>Status: picked up</span>
-          <span>
-            <button className={styles.button}>Take</button>
-          </span>
-        </div>
-        <div className={styles.parcel}>
-          <span>Name: Jhone Deo</span>
-          <span>Pick-up: London </span>
-          <span>Dropp-off: NY</span>
-          <span>Biker: 1</span>
-          <span>Status: picked up</span>
-          <span>
-            <button className={styles.button}>Take</button>
-          </span>
-        </div>
+        {parcels.map((parcel) => {
+          return (
+            <div className={styles.parcel} key={parcel.id}>
+              <span>Name: {parcel.name}</span>
+              <span>Pick-up: {parcel.pickupAddress}</span>
+              <span>Dropp-off: {parcel.dropoffAddress}</span>
+              <span>Biker: {parcel?.biker || "-"}</span>
+              <span>Status: picked up</span>
+              <span>
+                <button className={styles.button}>Take</button>
+              </span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
